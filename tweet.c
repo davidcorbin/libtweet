265f228dffb2df1c753824c7ff88b9967bcf3070
daconex <daconex@gmail.com>
2014-08-28 00:23:53 -0400

    Better code scheme

--- tweet.c
+++ tweet.c
@@ -6,83 +6,106 @@
 
 #include "tweet.h"
 
+
+/* CURL response callback */
 static size_t
 WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-  size_t realsize = size * nmemb;
-  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+	size_t realsize = size * nmemb;
+	struct Memory *mem = (struct Memory *)userp;
 
-  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
-  if(mem->memory == NULL) {
-    /* out of memory! */
-    printf("not enough memory (realloc returned NULL)\n");
-    return 0;
-  }
+	mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+	if(mem->memory == NULL) {
+		printf("Error allocating memory\n");
+		return 0;
+	}
+	memcpy(&(mem->memory[mem->size]), contents, realsize);
+	mem->size += realsize;
+	mem->memory[mem->size] = 0;
 
-  memcpy(&(mem->memory[mem->size]), contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
+	return realsize;
+}
 
-  return realsize;
+/* Show command arguments */
+void 
+show_args() {
+	printf("These are the arguments\n");
+	exit(1);
+}
+
+/* Show home feed */
+void homefeed() {
+
+}
+
+/* Parse the arguments */
+void 
+parse_args(int argc, char **argv) {
+	/* Only one argument */
+	if (argc == 1) {
+		show_args();
+	}
+	
+	/* If help menu */
+	if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+		show_args();
+	}
+	
+	else if (strcmp(argv[1], "home") == 0) {
+		homefeed();
+	}
 }
 
 
-int main(void)
+int 
+main(int argc, char **argv)
 {
-  CURL *curl_handle;
-  CURLcode res;
+	parse_args(argc, argv);
 
-  struct MemoryStruct chunk;
+	chunk.memory = malloc(1); // Will be reallocated
+	chunk.size = 0; // Nothing currently
 
-  chunk.memory = malloc(1); // Will be reallocated
-  chunk.size = 0; // Nothing currently
+	char *url = "https://api.twitter.com/1.1/statuses/home_timeline.json";
 
-char *url = "https://api.twitter.com/1.1/statuses/home_timeline.json";
+	char *signedurl = oauth_sign_url2(url, NULL, OA_HMAC, "GET", consumer_key, consumer_secret, user_token, user_secret);
 
-char *signedurl = oauth_sign_url2(url, NULL, OA_HMAC, "GET", consumer_key, consumer_secret, user_token, user_secret);
+	curl_global_init(CURL_GLOBAL_ALL);
 
+	/* Init CURL */
+	curl = curl_easy_init();
 
-  curl_global_init(CURL_GLOBAL_ALL);
+	/* Use the OAuth signed URL */
+	curl_easy_setopt(curl, CURLOPT_URL, signedurl);
 
-  /* init the curl session */
-  curl_handle = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 
-  /* specify URL to get */
-  curl_easy_setopt(curl_handle, CURLOPT_URL, signedurl);
+	/* Add response to memory */
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 
-curl_easy_setopt(curl_handle, CURLOPT_SSL_VERIFYPEER, 0L);
+	/* Give chunk to callback */
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 
-  /* send all data to this function  */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+	/* Execute curl */
+	res = curl_easy_perform(curl);
 
-  /* we pass our 'chunk' struct to the callback function */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+	/* Show Error */
+	if(res != CURLE_OK) {
+		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+	}
+	else {
+		//printf("%s", chunk.memory);
+		printf("Done");
+	}
 
-  /* some servers don't like requests that are made without a user-agent
-     field, so we provide one */
-  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "twittercli");
+	/* Don't leave a mess */
+	curl_easy_cleanup(curl);
 
-  /* get it! */
-  res = curl_easy_perform(curl_handle);
+	/* Free allocated memory */
+	if(chunk.memory)
+		free(chunk.memory);
 
-  /* check for errors */
-  if(res != CURLE_OK) {
-    fprintf(stderr, "curl_easy_perform() failed: %s\n",
-            curl_easy_strerror(res));
-  }
-  else {
+	/* Done with curl */
+	curl_global_cleanup();
 
-printf("%s", chunk.memory);
-  }
-
-  /* cleanup curl stuff */
-  curl_easy_cleanup(curl_handle);
-
-  if(chunk.memory)
-    free(chunk.memory);
-
-  /* we're done with libcurl, so clean it up */
-  curl_global_cleanup();
-
-  return 0;
+	return 0;
 }
--- tweet.h
+++ tweet.h
@@ -3,7 +3,12 @@ char *consumer_secret = "9OqFOSLP6W93jACsChkEfi3PYp4XLlBjI6Ry5COCnyO6DWnGhR";
 char *user_token = "620331547-x2XviwVplG1rawauTMgOKq4Mew4MyddEbGDCXnQ2";
 char *user_secret = "mNKJFF7wlI2Um1XU5Y5cpS90Iq1uV0x8Zi4HfBbCZskTz";
 
-struct MemoryStruct {
-  char *memory;
-  size_t size;
+struct Memory {
+	char *memory;
+	size_t size;
 };
+
+CURL *curl;
+CURLcode res;
+
+struct Memory chunk;
