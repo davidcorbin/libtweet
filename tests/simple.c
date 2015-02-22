9ad53daeb17ab2f83bc40afb9bb8c2da7e94fd40
David Corbin <daconex@gmail.com>
2015-02-22 21:25:19 +0000

    Added LICENSE pointing to COPYING

--- tests/simple.c
+++ tests/simple.c
@@ -1,3 +1,14 @@
+/*
+ *
+ * Copyright (c) 2015 David Corbin.
+ *
+ * This library is free software; 
+ * you can redistribute it and/or modify
+ * it under the terms of the MIT license. 
+ * See COPYING for details.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
